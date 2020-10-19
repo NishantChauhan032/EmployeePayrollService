6ca58dc3aab08ff52b6e3356949a8f0dc8363f00
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-19 18:09:01 +0530

    [NISHANT] add : ability to read and write employee payroll to a console

---
+++ src/main/java/com/capg/JUnit/employeePayroll/EmployeePayrollData.java
@@ -0,0 +1,20 @@
+package com.capg.JUnit.employeePayroll;
+
+public class EmployeePayrollData {
+	public String name;
+	public int id;
+	public double salary;
+
+	public EmployeePayrollData(int id, String name, double salary) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee ID = " + id + ", Employee Name = " +  name  + ", Salary = " + salary;
+	}
+
+}
